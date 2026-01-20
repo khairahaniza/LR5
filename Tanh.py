0eb9b6192f6c181ee9e61b588692ffe6e02d6499
khairahaniza <khaira11dien@gmail.com>
2026-01-21 00:17:13 +0800

    Refactor Tanh.py for improved readability

--- Tanh.py
+++ Tanh.py
@@ -7,15 +7,12 @@ st.set_page_config(page_title="Tanh Activation Function")
 st.title("Hyperbolic Tangent (Tanh)")
 st.write("Tanh maps input values to a range between -1 and 1.")
 
-# Define Tanh function
 def tanh(x):
     return np.tanh(x)
 
-# Generate input values
 x = np.linspace(-10, 10, 400)
 y = tanh(x)
 
-# Plot
 fig, ax = plt.subplots()
 ax.plot(x, y)
 ax.set_xlabel("Input")
@@ -24,3 +21,4 @@ ax.set_title("Tanh Activation Function")
 ax.grid(True)
 
 st.pyplot(fig)
+
